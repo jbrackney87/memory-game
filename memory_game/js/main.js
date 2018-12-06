aad454040c06e55feb9a8574f626583b36c3e0c5
Jake Brackney <brackney.jake@gmail.com>
2018-12-06 14:52:59 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,33 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardOne = cards[0];
+cardsInPlay.push("cardOne");
+	console.log("User Flipped Queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push("cardTwo");
+	console.log("User Flipped King");
+
+var cardThree = cards[1];
+cardsInPlay.push("cardThree");
+	console.log("User Flipped Queen");
+
+var cardFour = cards[2];
+cardsInPlay.push("cardFour");
+	console.log("User Flipped King");
+
+var checkForMatch = function () {	
+	if (cardsInPlay[0]) === cardsInPlay[1] {
+		alert("You found a Match!"); 
+		} else  {
+			alert("Sorry, try again.");
+		}
+		
+
+
+	
+
+
+
+
