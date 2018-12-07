f7f6ef82c6fe210f341dc7021caf892570199b1d
Jake Brackney <brackney.jake@gmail.com>
2018-12-07 13:54:19 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,31 @@
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+{	
+	rank: "Queen",
+	suit: "Hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "Queen",
+	suit: "Diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "King",
+	suit: "Hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "King",
+	suit: "Diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2);
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+		if (cardsInPlay[0] === cardsInPlay[2]) {
 			alert('You found a match!');
 		} 	else {
 			alert('Sorry, try again!'); 
@@ -11,12 +33,16 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-	console.log("User Flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId])
+	console.log("User Flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank)
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
 	}
 };
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
+
+
